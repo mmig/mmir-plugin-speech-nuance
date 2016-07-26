f2e407430c1e4084d42e63d9ecc8a655fffe6522
Patrick Bitterling <patrick.bitterling@dfki.de>
2016-07-26 21:59:40 +0200

    ios
    note
    para bug was and is not fixed
    
    new:
    you can use old app-key format in ios (all android win ios)

--- src/ios/de/nuancespeechplugin/Credentials.swift
+++ src/ios/de/nuancespeechplugin/Credentials.swift
@@ -17,21 +17,36 @@ class Credentials: NSObject {
     var serverUrl: String = "";
     var serverPort: String = "";
     
+    
     init(settings: [NSObject : AnyObject]) {
         super.init()
         self.serverHost = (settings["nuanceServerUrl".lowercaseString] as! String)
         self.serverPort = (settings["nuanceServerPort".lowercaseString] as! String)
         self.appId = (settings["nuanceAppId".lowercaseString] as! String)
-        self.appKey = (settings["nuanceAppKey".lowercaseString] as! String)
+        self.appKey = parseToNuanceKey(settings["nuanceAppKey".lowercaseString] as! String)
         //only needed for speechkit build
         SKSAppKey = self.appKey
         SKSAppId = self.appId
         SKSServerPort = self.serverPort
         SKSServerHost = self.serverHost
-        self.serverUrl = "nmsps://\(self.appId)@\(self.serverHost):\(self.serverPort)"
+        self.serverUrl = "nmsps://"+self.appId+"@"+self.serverHost+":"+self.serverPort
         SKSServerUrl = self.serverUrl
     }
 
+    func parseToNuanceKey (confStr : String) -> String {
+        var tmpStr = confStr
+        if tmpStr.containsString("0x") {
+            tmpStr = tmpStr.stringByReplacingOccurrencesOfString("(byte)", withString: "")
+            tmpStr = tmpStr.stringByReplacingOccurrencesOfString(" ", withString: "")
+            tmpStr = tmpStr.stringByReplacingOccurrencesOfString("0x", withString: "")
+            tmpStr = tmpStr.stringByReplacingOccurrencesOfString(",", withString: "")
+            tmpStr = tmpStr.stringByReplacingOccurrencesOfString("{", withString: "")
+            tmpStr = tmpStr.stringByReplacingOccurrencesOfString("}", withString: "")
+        }
+
+        NSLog("Appkey Output: %@ ", tmpStr )
+        return tmpStr
+    }
     
     
 }
